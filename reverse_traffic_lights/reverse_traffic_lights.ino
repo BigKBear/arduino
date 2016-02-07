4310d920c27a917470c9683a870732bf372a5bba
BigKBear <kylesthill77@gmail.com>
2016-02-07 19:46:39 -0400

    adding the outline for reverse traffic lights code

---
+++ reverse_traffic_lights/reverse_traffic_lights.ino
@@ -0,0 +1,32 @@
+/*
+*Reversed traffic lights
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // car motion start on all 4 roads
+  
+  //Road 1
+    //Road1 straight and right
+
+    //Road1 left
+    
+  //Road 2
+    //Road2 straight and right
+
+    //Road2 left
+    
+  //Road 3
+    //Road3 straight and right
+
+    //Road3 left
+    
+  //Road 4
+    //Road4 straight and right
+
+    //Road4 left
+}
